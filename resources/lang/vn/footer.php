e66e11eed79235b18cf321563c4666b6216b0a73
khanhpham10 <khanhphamlight@gmail.com>
2021-01-09 20:22:33 +0700

    src web

---
+++ resources/lang/vn/footer.php
@@ -0,0 +1,14 @@
+<?php
+
+  return [
+    'AboutUs'          =>      'Về Chúng Tôi',
+    'address'          =>      '172B Đường 3/2, Phường Hưng Lợi, Quận Ninh Kiều, Thành phố Cần Thơ',
+    'FollowUs'         =>      'Theo Dõi Chúng Tôi',
+    'visa'             =>      'Thanh toán bằng thẻ Visa.',
+    'mastercard'       =>      'Thanh toán bằng thẻ MasterCard.',
+    'jcb'              =>      'Thanh toán bằng thẻ JCB.',
+    'cod'              =>      'Thanh toán khi nhận hàng.',
+    'nganluong'        =>      'Giải pháp thanh toán qua Ngân Lượng.',
+    'payment'          =>      'Hỗ Trợ Thanh Toán',
+    'support'          =>      'Hỗ Trợ Khách Hàng',
+  ];
