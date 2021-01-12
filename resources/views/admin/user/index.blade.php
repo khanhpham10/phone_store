ebfd34f9f2e8d532eee719811d6facd37714f321
khanhpham10 <khanhphamlight@gmail.com>
2021-01-12 18:33:12 +0700

    đã chỉnh sữa

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -113,7 +113,7 @@
                 <th data-orderable="false">Địa Chỉ</th>
                 <th data-width="60px" data-type="date-euro">Ngày Tạo</th>
                 <th data-width="66px">Trạng Thái</th>
-                <th data-orderable="false" data-width="70px">Tác Vụ</th>
+                <th data-orderable="false" data-width="70px">Sửa_Xóa</th>
               </tr>
             </thead>
             <tbody>
